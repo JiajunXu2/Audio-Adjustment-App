f591bc22f7dcf9568b4fe150b439df758efe25d8
Jiajun Xu <jiajunxu2@gmail.com>
2024-03-07 19:16:08 -0500

    testing window and tab functions

--- tab_control.py
+++ tab_control.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
+import time
 
 # list of distracting sites to limit
 restricted_urls = ["https://www.reddit.com/",
                    ]
 
 chrome = webdriver.Chrome()
-print(chrome.window_handles)
-#chrome.switch_to.window(chrome.window_handles)
-#chrome.get("https://www.youtube.com/")
-
+#chrome.switch_to(chrome.window_handles[0])
+# chrome.switch_to.window(chrome.window_handles)
+chrome.get("https://www.youtube.com/")
+print(chrome.title)
 while True:
-    pass
+    if chrome.title == "Reddit":
+        chrome.close()
+    print(chrome.title)
+    time.sleep(5)
+
+# .send_keys(Keys.CONTROL + 'w')
+

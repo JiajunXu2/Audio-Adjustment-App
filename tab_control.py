f86a2310104b992c8cf17ff8bd101ee787b8f28e
Jiajun Xu <jiajunxu2@gmail.com>
2024-03-12 20:02:43 -0400

    tries to make newly opened tab the active tab

--- tab_control.py
+++ tab_control.py
@@ -1,21 +1,28 @@
 from selenium import webdriver
+from urllib.parse import urlparse
 import time
 
 # list of distracting sites to limit
-restricted_urls = ["https://www.reddit.com/",
-                   "https://www.instagram.com/",
-                   "https://www.tiktok.com/",
-                   "https://twitter.com/",
-                   "https://www.facebook.com/",
+restricted_urls = ["www.reddit.com",
+                   "www.instagram.com",
+                   "www.tiktok.com",
+                   "twitter.com",
+                   "www.facebook.com",
                    ]
-
+"""
+for urls in restricted_urls:
+    domain = urlparse(urls).netloc
+    print(domain)
+"""
+ 
 chrome = webdriver.Chrome()
 chrome.get("https://www.google.com/")
 while True:
-    if chrome.title in restricted_urls:
+    active_window = chrome.switch_to.window(chrome.window_handles[0])
+    domain = urlparse(active_window.current_url)
+    if domain in restricted_urls:
         chrome.close()
-    print(chrome.title)
+    
     time.sleep(2)
 
-# .send_keys(Keys.CONTROL + 'w')
 
